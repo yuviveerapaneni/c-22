704ea50b8d905184b0371c4acc18ba1ffcf55de2
yuviveerapaneni <87616916+yuviveerapaneni@users.noreply.github.com>
2022-07-20 18:42:22 -0500

    Add files via upload

---
+++ project-C22-template-main/sketch.js
@@ -0,0 +1,54 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var baseimage;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES);
+
+  var options = {
+    isStatic: true
+  };
+
+  //create player base body
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
+  //create player body
+player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+World.add(world,player)
+
+
+}
+
+function draw() {
+  background(backgroundImg);
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
+  //show the player image using image() function
+
+  //show the playerbase image using image() function
+
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+}
